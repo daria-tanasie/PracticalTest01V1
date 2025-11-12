23e7ef5bb2edd86f32acda5bd98eaa0475035b09
daria-tanasie <tanasiedaria@gmail.com>
2025-11-12 12:19:46 +0200

    ex B

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01v1/PracticalTest01MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01v1/PracticalTest01MainActivity.kt
@@ -3,14 +3,57 @@ package ro.pub.cs.systems.eim.practicaltest01v1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PracticalTest01MainActivity : AppCompatActivity() {
+
+    private lateinit var input1: EditText
+    private lateinit var input2: EditText
+
+    private var lNumber = 0
+    private var rNumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_practical_test01_main)
 
+        input1 = findViewById(R.id.input1)
+        input2 = findViewById(R.id.input2)
+        input1.setText("0")
+        input2.setText("0")
+
+
+        val pressMe = findViewById<Button>(R.id.press_me)
+        pressMe.setOnClickListener {
+            lNumber++
+            input1.setText(lNumber.toString())
+        }
+
+        val pressMeToo = findViewById<Button>(R.id.press_me_too)
+        pressMeToo.setOnClickListener {
+            rNumber++
+            input2.setText(rNumber.toString())
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("INPUT1", input1.text.toString())
+        outState.putString("INPUT2", input2.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.containsKey("INPUT1") && savedInstanceState.containsKey("INPUT2")) {
+            input1.setText(savedInstanceState.getString("INPUT1"))
+            input2.setText(savedInstanceState.getString("INPUT2"))
+            lNumber = Integer.valueOf(input1.text.toString())
+            rNumber = Integer.valueOf(input2.text.toString())
+        }
     }
 }
